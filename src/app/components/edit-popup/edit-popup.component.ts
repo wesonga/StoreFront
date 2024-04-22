e00c31a460181732b245738909a728f4e36fb0f9
Wesonga Bob <wesongabob1@yahoo.com>
2024-04-22 23:49:08 +0300

    Create CRUD endpoints, functions and buttons

---
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../../types';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
+
+@Component({
+  selector: 'app-edit-popup',
+  standalone: true,
+  imports: [
+    DialogModule,
+    CommonModule,
+    FormsModule,
+    RatingModule,
+    ButtonModule,
+  ],
+  templateUrl: './edit-popup.component.html',
+  styleUrl: './edit-popup.component.scss',
+})
+export class EditPopupComponent {
+  @Input() display: boolean = false;
+  @Output() displayChange = new EventEmitter<boolean>();
+  @Input() header!: string;
+
+  @Input() product: Product = {
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+  @Output() confirm = new EventEmitter<Product>();
+
+  onConfirm() {
+    this.confirm.emit(this.product);
+    this.display = false;
+    this.displayChange.emit(this.display);
+  }
+
+  onCancel() {
+    this.display = false;
+    this.displayChange.emit(this.display);
+  }
+}
